6bf87a81967d156821a0fa2720e7ef23c4c66943
codyccarmona <76072605+codyccarmona@users.noreply.github.com>
2022-04-11 14:28:32 -0400

    read lines
    made sure main is accepting all command line arguments. used first command line argument to open file and read lines

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,13 +1,39 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
+
+struct Vertex{
+    Vertex() : d(__DBL_MAX__), pi(nullptr) {}
+    
+    void setd(double d){ this->d = d; }
+private:
+    double d;
+    Vertex* pi;
+};
+
+struct Graph{
+    Graph(){}
+    Graph(int size) : vertices(std::vector<Vertex>(size, Vertex())) {}
+private:
+    std::vector<Vertex> vertices;
+};
+
+std::vector<std::string> readLines(std::string file){
+    std::ifstream srcFile(file);
+    std::string readLine;
+    std::vector<std::string> lines;
+
+    while(std::getline(srcFile, readLine))
+        lines.push_back(readLine);
+
+    srcFile.close();
+
+    return lines;
+}
 
 int main(int argc, char *argv[]){
-    std::vector<std::string> lines;
-    std::string readLine;
-
-    while(std::getline(std::cin, readLine))
-        lines.push_back(readLine);
+    std::vector<std::string> lines = readLines(argv[1]);
 
     return 0;
 }
