05b59429a5fc92bd077de7869b034b2fe11a4587
codyccarmona <76072605+codyccarmona@users.noreply.github.com>
2022-04-13 10:28:09 -0400

    Reset Branch
    Attempting a different implementation using adjacency lists and visited tables

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,29 +1,5 @@
-#include <fstream>
 
-#include <bits/stdc++.h>
-#include "priority_queue.h"
-using namespace std;
-
-void dijkstra(Graph& g){
-
-}
 
 int main(int argc, char *argv[]){
-    ifstream file(argv[1]);
-    Graph G;
-    vector<pair<string, string>> lines;
-    string vertexCount;
-    
-    getline(file, vertexCount);
-    lines = vector<pair<string, string>>(stoi(vertexCount));
-
-    for(int i = 0; i < stoi(vertexCount); i++){
-        getline(file, lines[i].first, ' ');
-        getline(file, lines[i].second);
-    }
-
-    file.close();
-
-    G = Graph(lines, argv[2]);    
     return 0;
 }
